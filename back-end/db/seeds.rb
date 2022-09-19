a2ba95cf17f27bb9cc01f505be1daafcd38ebe2a
Mustafa Binalhag <mbin828@gmail.com>
2022-09-19 11:49:55 -0400

    user model

--- back-end/db/seeds.rb
+++ back-end/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "deleting all data"
+User.destroy_all
+
+puts "creating users"
+10.times do
+    name = Faker::JapaneseMedia::OnePiece.character
+    new_user = User.create!(username: name, email: "#{name.delete(' ')}@bounty.com", password: "123", tag: Faker::JapaneseMedia::OnePiece.island)
+end
+
+puts "seeded successfully 👒🏴‍☠️"
