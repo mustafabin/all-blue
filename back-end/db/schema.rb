f1310845b6d3c53bf185ac20488ae963037ac4fb
Mustafa Binalhag <mbin828@gmail.com>
2022-09-27 16:32:00 -0400

    render ready

--- back-end/db/schema.rb
+++ back-end/db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_21_140319) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "banned_ips", force: :cascade do |t|
     t.string "client_ip"
     t.datetime "created_at", null: false
