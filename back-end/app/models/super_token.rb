73365010fd801b6f69d1a9b4cc618f6a7da3a3cf
Mustafa Binalhag <mbin828@gmail.com>
2022-12-01 19:03:38 -0500

    got rid of token limit

--- back-end/app/models/super_token.rb
+++ back-end/app/models/super_token.rb
@@ -1,59 +1,41 @@
 class SuperToken < ApplicationRecord
     validates_uniqueness_of :token
     belongs_to :user
-    # if no limit is desired then set const to 0
-    # avoid changing this number but if changed all tokens prior to change will be deleted ordered by usage (the most active token stays)
-    LIMIT_TOKENS_PER_USER = 2
     AUTO_REFRESH = false
     DAYS_TO_EXPIRE = 14
     def self.generate_token(user,request)
         if user && request
             all_tokens = SuperToken.where(user_id: user.id)
-            # if there is a limit destroy the least active token(s)
-            if LIMIT_TOKENS_PER_USER != 0 && all_tokens.length >= LIMIT_TOKENS_PER_USER
-                if all_tokens.length > LIMIT_TOKENS_PER_USER
-                    # Because the amount of active tokens exceeds the limit
-                    # All of oldest tokens up to the limit must be deleted 
-                    # This happens when the const LIMIT_TOKENS_PER_USER is changed from last start up
-                    # Due to the limit being variable we must order the tokens first and grab the latest valid time 
-                    # and deleting all the tokens older than the oldest valid token (the younger the token the most recently it has been used)
-                    last_valid_time = all_tokens.order("updated_at DESC").slice(0,LIMIT_TOKENS_PER_USER-1).last.updated_at
-                    all_tokens.where("updated_at < ?", last_valid_time).destroy_all
-                else
-                    all_tokens.order("updated_at")[0].destroy
-                end
-            end
             # generate hash https://github.com/rails/rails/blob/main/activerecord/lib/active_record/secure_token.rb
             hash = SecureRandom.base58(36)
             # generate token based off user
             SuperToken.create!(token:hash, user_id: user.id, client_ip: request.remote_ip, agent: request.user_agent, expiry: Time.now)
         else 
-            raise "user and/or request arguments undefined"
+            return {status: "400", error:"Bad request", message:"user and/or request arguments undefined"}
         end
     end
     def self.vaildate_super request
-        token = request.headers["SuperToken"] 
+        token = request.headers["SuperToken"]
         if !token
-            return {status: "bad", error:"SuperToken Header Found", message:"header needs to called SuperToken not token or authentication"}
+            return {status: "400", error:"SuperToken Header Not Found", message:"Header needs to called SuperToken not anything else"}
         end
-        super_token = SuperToken.find_by(token:token)
-        if !super_token
-            return {status: "bad", error:"SuperToken Not Found", message:"Token doesnt exist in database"}
-        end
-        if super_token.client_ip == request.remote_ip
-            if is_expired super_token.expiry.to_i
+        super_token = SuperToken.find_by!(token:token)
+        if super_token.agent == request.user_agent
+            if is_expired(super_token.expiry.to_i)
                 super_token.destroy 
-                {status: "bad", error:"401 not authorized", message:"EXPIRED TOKEN"}
+                {status: "401", error:"401 not authorized", message:"EXPIRED TOKEN"}
             else
                 if AUTO_REFRESH 
+                    # updates lifespan of token
                     super_token.update(expiry: Time.now)
                 else
+                    # doesnt update lifespan but this is here b/c updated_at is how to track the lastest used token
                     super_token.update(updated_at: Time.now)
                 end
-                 {status: "ok", user:super_token.user.profile}
+                 {status: "200", user:super_token.user}
             end
         else
-            {status: "bad", error:"403 forbidden", message:"DIFFERENT DEVICE "}
+            {status: "403", error:"403 forbidden", message:"DIFFERENT DEVICE "}
         end
     end
 
